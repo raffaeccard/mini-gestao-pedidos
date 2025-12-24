735a4080b7a48f4e48b4d4131e7db3d9a73abfcf
Raffael Eccard <raffaelceccard@gmail.com>
2025-12-24 15:07:19 -0300

    iniciando projeto mini aplicacao de gestao de produtos

---
+++ backend/src/main/java/com/app/mini_gestao_pedidos/MiniGestaoPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.app.mini_gestao_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGestaoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniGestaoPedidosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/mini_gestao_pedidos/MiniGestaoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mini_gestao_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniGestaoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
